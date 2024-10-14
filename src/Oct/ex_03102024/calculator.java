e43a615fd81643ea21f63c46b24ce43d349de258
meenapunna <meenap@invenco.com>
2024-10-14 16:22:12 +0530

    calculator

--- src/Oct/ex_03102024/calculator.java
+++ src/Oct/ex_03102024/calculator.java
@@ -1,4 +1,13 @@
 package Oct.ex_03102024;
 
 public class calculator {
+    public static void main(String[] args) {
+        int a=5;
+        int b=10;
+        int c;
+        c=a+b;
+        System.out.println(c);
+        System.out.println(10+5);
+        System.out.println(10/2);
+    }
 }
